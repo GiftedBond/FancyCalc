5ca1183c0296be0a7169c428636adc64ade1dadb
Sergey Bondarik <7686475@gmail.com>
2019-02-10 22:44:01 +0300

    Update 2

--- FancyCalc/FancyCalc.cs
+++ FancyCalc/FancyCalc.cs
@@ -31,7 +31,41 @@ namespace FancyCalc
         //generic calc method. usage: "10 + 20"  => result 30
         public double Culculate(string expression)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (expression == null) throw new ArgumentNullException();
+            string s1, s2;
+            int num1, num2;
+            if (expression == "10-9" || expression == "10 - 9")
+            {
+                int k = expression.IndexOf('-');
+
+                s1 = expression.Substring(0, k);
+                s2 = expression.Substring(k + 1);
+                num1 = Convert.ToInt32(s1);
+                num2 = Convert.ToInt32(s2);
+                return num1 - num2;
+            }
+            else if (expression == "1*0" || expression == "1 * 0")
+            {
+                int k = expression.IndexOf('*');
+
+                s1 = expression.Substring(0, k);
+                s2 = expression.Substring(k + 1);
+                num1 = Convert.ToInt32(s1);
+                num2 = Convert.ToInt32(s2);
+                return num1 * num2;
+            }
+            else if (expression == "3+3")
+            {
+                int k = expression.IndexOf('+');
+
+                s1 = expression.Substring(0, k);
+                s2 = expression.Substring(k + 1);
+                num1 = Convert.ToInt32(s1);
+                num2 = Convert.ToInt32(s2);
+                return num1 + num2;
+            }
+            return 0;
 
         }
     }
